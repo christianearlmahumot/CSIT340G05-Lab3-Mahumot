c6bf481056b9b368fca086b5c9092b3702952a75
Christian Earl V. Mahumot <earlchristian1605@gmail.com>
2026-09-21 20:13:35 +0800

    1.1: course information step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,63 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return <p>Number of units {props.total}</p>
+}
+
+const Footer = (props) => {
+  return (
+    <footer>
+      <p>
+        {props.name} - {props.courseCode} - {props.section}
+      </p>
+    </footer>
+  )
+}
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = 'CSIT340 Industry Elective 1'
+  const part1 = 'IT332 Capstone and Research 1'
+  const exercises1 = 3
+  const part2 = 'CSIT335 Testing and Quality Assurance'
+  const exercises2 = 3
+  const part3 = 'IT386 Information Assurance and Security 2'
+  const exercises3 = 3
+
+  const studentName = 'Christian Earl V. Mahumot'
+  const courseCode = 'CSIT340'
+  const section = 'G01'
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
+      <Footer name={studentName} courseCode={courseCode} section={section} />
     </div>
   )
 }
